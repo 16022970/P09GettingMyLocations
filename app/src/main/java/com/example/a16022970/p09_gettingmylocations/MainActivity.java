945bf0092e5257e5c33c54080dd9ce1290cc0e19
16022970 <16022970@myrp.edu.sg>
2018-07-17 19:02:21 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a16022970/p09_gettingmylocations/MainActivity.java
@@ -0,0 +1,134 @@
+package com.example.a16022970.p09_gettingmylocations;
+
+import android.Manifest;
+import android.content.Intent;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.PermissionChecker;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnStart, btnStop, btnCheck;
+    TextView tvResult;
+
+    LocationCallback mLocationCallback;
+    FusedLocationProviderClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnStart = (Button)findViewById(R.id.btnStart);
+        btnStop = (Button)findViewById(R.id.btnStop);
+        btnCheck = (Button)findViewById(R.id.btnCheck);
+
+        tvResult = (TextView)findViewById(R.id.tvResult);
+        client = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+
+
+        checkPermission();
+
+        if (checkPermission() == true) {
+            Task<Location> task = client.getLastLocation();
+            task.addOnSuccessListener(MainActivity.this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null) {
+                        tvResult.setText("Last known location when this Activity started: \nLatitude: " + location.getLatitude() + "\nLongitude: " + location.getLongitude());
+                        Toast.makeText(MainActivity.this, "Last known location shown", Toast.LENGTH_SHORT).show();
+                    } else {
+                        String msg = "No Last Known Location found";
+                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        } else {
+            String msg = "Permission not granted to retrieve location info";
+            Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
+        }
+
+//        mLocationCallback = new LocationCallback() {
+//            @Override
+//            public void onLocationResult(LocationResult locationResult) {
+//                if (locationResult != null) {
+//                    Location data = locationResult.getLastLocation();
+//
+//                    String lat = String.valueOf(data.getLatitude());
+//                    String lng = String.valueOf(data.getLongitude());
+//
+//                    String msg = "New Loc Detected\nLat: " + lat + ",Lng: " + lng;
+//                    Toast.makeText(MainActivity.this,msg,Toast.LENGTH_SHORT).show();
+//                }
+//            };
+//        };
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkPermission() == true) {
+                    LocationRequest mLocationRequest = LocationRequest.create();
+                    mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+                    mLocationRequest.setInterval(10000);
+                    mLocationRequest.setFastestInterval(5000);
+                    mLocationRequest.setSmallestDisplacement(100);
+
+                    client.requestLocationUpdates(mLocationRequest, mLocationCallback, null);
+
+                } else {
+                    String msg = "Permission not granted to retrieve location info";
+                    Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
+                }
+                
+                Intent i = new Intent(MainActivity.this, MyService.class);
+                startService(i);
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, MyService.class);
+                stopService(i);
+            }
+        });
+
+        btnCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+    //check required permissions
+    private boolean checkPermission() {
+        int permissionCheck_Coarse = ContextCompat.checkSelfPermission(
+                MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION);
+        int permissionCheck_Fine = ContextCompat.checkSelfPermission(
+                MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if (permissionCheck_Coarse == PermissionChecker.PERMISSION_GRANTED
+                || permissionCheck_Fine == PermissionChecker.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
