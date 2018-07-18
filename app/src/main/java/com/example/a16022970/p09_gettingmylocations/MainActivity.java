6a025dd004bcfbdb358b93775f8eb19007a12feb
16022970 <16022970@myrp.edu.sg>
2018-07-18 23:58:51 +0800

    done 1

--- app/src/main/java/com/example/a16022970/p09_gettingmylocations/MainActivity.java
+++ app/src/main/java/com/example/a16022970/p09_gettingmylocations/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.a16022970.p09_gettingmylocations;
 
 import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -18,117 +22,196 @@ import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
-public class MainActivity extends AppCompatActivity {
-    Button btnStart, btnStop, btnCheck;
-    TextView tvResult;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
-    LocationCallback mLocationCallback;
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvlastLocation,tvlat,tvlng;
+    Button btnStart,btnStop,btnCheck;
     FusedLocationProviderClient client;
+    String folderLocation;
+    FragmentManager fm;
+    SupportMapFragment mapFragment;
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btnStart = (Button)findViewById(R.id.btnStart);
-        btnStop = (Button)findViewById(R.id.btnStop);
-        btnCheck = (Button)findViewById(R.id.btnCheck);
-
-        tvResult = (TextView)findViewById(R.id.tvResult);
-        client = LocationServices.getFusedLocationProviderClient(MainActivity.this);
-
-
-        checkPermission();
-
-        if (checkPermission() == true) {
-            Task<Location> task = client.getLastLocation();
-            task.addOnSuccessListener(MainActivity.this, new OnSuccessListener<Location>() {
-                @Override
-                public void onSuccess(Location location) {
-                    if (location != null) {
-                        tvResult.setText("Last known location when this Activity started: \nLatitude: " + location.getLatitude() + "\nLongitude: " + location.getLongitude());
-                        Toast.makeText(MainActivity.this, "Last known location shown", Toast.LENGTH_SHORT).show();
-                    } else {
-                        String msg = "No Last Known Location found";
-                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
-                    }
-                }
-            });
-        } else {
-            String msg = "Permission not granted to retrieve location info";
-            Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
-        }
-
-//        mLocationCallback = new LocationCallback() {
-//            @Override
-//            public void onLocationResult(LocationResult locationResult) {
-//                if (locationResult != null) {
-//                    Location data = locationResult.getLastLocation();
-//
-//                    String lat = String.valueOf(data.getLatitude());
-//                    String lng = String.valueOf(data.getLongitude());
-//
-//                    String msg = "New Loc Detected\nLat: " + lat + ",Lng: " + lng;
-//                    Toast.makeText(MainActivity.this,msg,Toast.LENGTH_SHORT).show();
-//                }
-//            };
-//        };
+        tvlastLocation = findViewById(R.id.tvlastLocation);
+        tvlat = findViewById(R.id.tvlat);
+        tvlng = findViewById(R.id.tvlng);
+        btnStart = findViewById(R.id.btnStart);
+        btnStop = findViewById(R.id.btnStop);
+        btnCheck = findViewById(R.id.btnCheck);
+        client = LocationServices.getFusedLocationProviderClient(this);
+        fm = getSupportFragmentManager();
+        mapFragment = (SupportMapFragment) fm.findFragmentById(R.id.map);
+        getLastLatLong();
+        createFolder();
 
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (checkPermission() == true) {
-                    LocationRequest mLocationRequest = LocationRequest.create();
-                    mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-                    mLocationRequest.setInterval(10000);
-                    mLocationRequest.setFastestInterval(5000);
-                    mLocationRequest.setSmallestDisplacement(100);
-
-                    client.requestLocationUpdates(mLocationRequest, mLocationCallback, null);
-
-                } else {
-                    String msg = "Permission not granted to retrieve location info";
-                    Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
-                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
+                if (checkPermission()) {
+                    Intent i = new Intent(MainActivity.this, MyService.class);
+                    startService(i);
+                }else {
+                    Toast.makeText(MainActivity.this, "Permission not granted", Toast.LENGTH_LONG).show();
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
                 }
-
-                Intent i = new Intent(MainActivity.this, MyService.class);
-                startService(i);
             }
         });
 
         btnStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, MyService.class);
-                stopService(i);
+                if (checkPermission()) {
+                    Intent i = new Intent(MainActivity.this, MyService.class);
+                    stopService(i);
+                }else {
+                    Toast.makeText(MainActivity.this, "Permission not granted", Toast.LENGTH_LONG).show();
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                }
             }
         });
 
         btnCheck.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Code for file reading
+                folderLocation = Environment.getExternalStorageDirectory().getAbsolutePath() + "/MyFolder";
+                File targetFile = new File(folderLocation, "data.txt");
 
+                if (targetFile.exists() == true) {
+                    String data = "";
+                    try {
+                        FileReader reader = new FileReader(targetFile);
+                        BufferedReader br = new BufferedReader(reader);
+
+                        String line = br.readLine();
+                        while (line != null) {
+                            data += line + "\n";
+                            line = br.readLine();
+                        }
+                        br.close();
+                        reader.close();
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        Toast.makeText(MainActivity.this, "Failed to read!", Toast.LENGTH_SHORT).show();
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(MainActivity.this,data,Toast.LENGTH_LONG).show();
+                    Intent i = new Intent(MainActivity.this,ReadFileActivity.class);
+                    startActivity(i);
+                    Log.d("content", data);
+                }
             }
         });
+    }
+
+    private void getLastLatLong(){
+        if (checkPermission()) {
+            Task<Location> task = client.getLastLocation();
+            task.addOnSuccessListener(MainActivity.this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(final Location location) {
+                    if (location != null) {
+                        tvlat.setText("Latitude : "+location.getLatitude());
+                        tvlng.setText("Longitude : "+location.getLongitude());
+                        mapFragment.getMapAsync(new OnMapReadyCallback() {
+                            @Override
+                            public void onMapReady(GoogleMap googleMap) {
+                                map = googleMap;
+
+                                LatLng poi_Singapore = new LatLng(1.352083, 103.819836);
+                                map.moveCamera(CameraUpdateFactory.newLatLngZoom(poi_Singapore, 10));
+
+                                UiSettings ui = map.getUiSettings();
+                                ui.setCompassEnabled(true);
+                                ui.setZoomControlsEnabled(true);
+
+                                LatLng mypoint = new LatLng(location.getLatitude(), location.getLongitude());
+                                Marker marker = map.addMarker(new
+                                        MarkerOptions()
+                                        .position(mypoint)
+                                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+                            }
+                        });
+                    } else {
+                        Toast.makeText(MainActivity.this, "No known Last Location found", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        } else {
+            Toast.makeText(MainActivity.this, "Permission not granted to retrieve location info", Toast.LENGTH_LONG).show();
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }
 
     }
-    //check required permissions
-    private boolean checkPermission() {
+    private void createFolder(){
+        if (checkPermission()){
+            folderLocation = Environment.getExternalStorageDirectory().getAbsolutePath() + "/MyFolder";
+            File folder = new File(folderLocation);
+            if (folder.exists() == false){
+                boolean result = folder.mkdir();
+                if (result == true){
+                    Log.d("File Read/Write", "Folder created");
+                }
+            }
+        }else{
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        }
+    }
+
+
+    public boolean checkPermission() {
         int permissionCheck_Coarse = ContextCompat.checkSelfPermission(
-                MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION);
+                this, Manifest.permission.ACCESS_COARSE_LOCATION);
         int permissionCheck_Fine = ContextCompat.checkSelfPermission(
                 MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION);
+        int read = ContextCompat.checkSelfPermission(
+                MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        int write = ContextCompat.checkSelfPermission(
+                MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
 
         if (permissionCheck_Coarse == PermissionChecker.PERMISSION_GRANTED
-                || permissionCheck_Fine == PermissionChecker.PERMISSION_GRANTED) {
+                || permissionCheck_Fine == PermissionChecker.PERMISSION_GRANTED || read == PermissionChecker.PERMISSION_GRANTED
+                || write == PermissionChecker.PERMISSION_GRANTED) {
             return true;
         } else {
             return false;
         }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getLastLatLong();
+                    createFolder();
+                }
+            }
+        }
+    }
+
 }
